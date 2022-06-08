5ac391a20f4451a5009e334e72cbab48998b63ea
Perisiguiendo <1987391831@qq.com>
2022-06-08 21:07:50 +0800

    fix: update defaultProps

--- src/dropdown-menu/defaultProps.ts
+++ src/dropdown-menu/defaultProps.ts
@@ -2,6 +2,7 @@
  * 该文件为脚本自动生成文件，请勿随意修改。如需修改请联系 PMC
  * */
 
+import identity from 'lodash/identity';
 import { TdDropdownMenuProps, TdDropdownItemProps } from './type';
 
 export const dropdownMenuDefaultProps: TdDropdownMenuProps = {
@@ -17,4 +18,5 @@ export const dropdownItemDefaultProps: TdDropdownItemProps = {
   optionsColumns: 1,
   optionsLayout: 'columns',
   value: undefined,
+  onChange: identity,
 };
