688bd577d51ff535b349ad4f96091ca809323df7
Perisiguiendo <1987391831@qq.com>
2022-06-08 21:09:06 +0800

    fix: update dropdown-menu & update example

--- src/dropdown-menu/DropdownMenu.tsx
+++ src/dropdown-menu/DropdownMenu.tsx
@@ -1,5 +1,5 @@
 import React, { memo, useState, useRef, ReactElement } from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { useSpring, animated, to } from '@react-spring/web';
 import classnames from 'classnames';
 import { useBoolean } from 'ahooks';
 import { Overlay } from 'tdesign-mobile-react';
@@ -7,34 +7,78 @@ import withNativeProps, { NativeProps } from '../_util/withNativeProps';
 import { TdDropdownMenuProps } from './type';
 import { dropdownMenuDefaultProps } from './defaultProps';
 import useConfig from '../_util/useConfig';
-import usePopupCssTransition from './hooks/usePopupCssTransition';
 
 export type DropdownMenuProps = TdDropdownMenuProps & NativeProps;
 
 const DropdownMenu: React.FC<DropdownMenuProps> = (props) => {
   const { zIndex, showOverlay, activeColor, closeOnClickOverlay, duration } = props;
+  /**
+   * 菜单栏index
+   */
+  const [curIndex, setCurIndex] = useState<number>(null);
+  /**
+   * 下拉框绝对高度
+   */
+  const [top, setTop] = useState<number>(0);
+  /**
+   * 遮罩层状态
+   */
+  const [visible, { setTrue, setFalse }] = useBoolean(false);
+  /**
+   * 当前菜单栏的层级
+   */
+  const zIndexComputed = visible ? zIndex || 10001 : 999;
+
+  const maskRef = useRef<HTMLDivElement>(null);
+  const menuRef = useRef<HTMLDivElement>(null);
 
   const { classPrefix } = useConfig();
   const name = `${classPrefix}-dropdown`;
   const menuName = `${name}-menu`;
   const itemName = `${name}-item`;
-  const [top, setTop] = useState<number>(0);
 
-  const [visible, { setTrue, setFalse }] = useBoolean(false);
+  // 蒙层动画
+  const maskSpring = useSpring({
+    opacity: visible ? 1 : 0,
+    config: {
+      duration: Number(+duration),
+    },
+    onStart: () => {
+      console.log('onStart: ');
+      // setTrue();
+    },
+    onRest: () => {
+      console.log('onRest: ');
+      // setFalse();
+    },
+  });
 
-  const maskRef = useRef<HTMLDivElement>(null);
+  /**
+   * 下拉层动画
+   */
+  const { o } = useSpring({
+    from: { o: 0 },
+    o: top,
+    config: {
+      duration: 120000,
+    },
+    onStart: () => {
+      console.log('onStart: ');
+      // setTrue();
+    },
+    onRest: () => {
+      console.log('onRest: ');
+      // setFalse();
+    },
+  });
 
-  const menuRef = useRef<HTMLDivElement>(null);
-
-  const [curIndex, setCurIndex] = useState<number>(null);
-
-  const maskCssTransitionState = usePopupCssTransition({ contentRef: maskRef, classPrefix: 'fade' });
-
-  const newChildren = React.Children.map(props.children, (item) => item);
-  console.log('newChildren: ', newChildren);
-
+  /**
+   * 子元素
+   */
+  const newChildren = React.Children.map(props.children, (item) =>
+    React.cloneElement(item, { hide: setFalse, clearIndex: setCurIndex, duration, visible }),
+  );
   const items = newChildren.filter((item: ReactElement, index) => index === curIndex);
-
   /**
    * 设置当前选中的index
    * 点击菜单后，再去获取菜单元素的top值，然后设置到expand中
@@ -51,7 +95,9 @@ const DropdownMenu: React.FC<DropdownMenuProps> = (props) => {
     }
     setCurIndex(index === curIndex ? null : index);
   };
-
+  /**
+   * 点击遮罩层
+   */
   const handleOverlayClick = () => {
     if (closeOnClickOverlay) {
       setFalse();
@@ -59,17 +105,20 @@ const DropdownMenu: React.FC<DropdownMenuProps> = (props) => {
     }
   };
 
-  const zIndexComputed = visible ? zIndex || 10001 : 999;
-
   return withNativeProps(
     props,
     <div className={menuName}>
-      <CSSTransition in={visible} timeout={duration} appear {...maskCssTransitionState.props}>
-        <div ref={maskRef} className={`${itemName}`} style={{ display: 'none' }}>
+      <animated.div style={maskSpring}>
+        <div ref={maskRef} className={`${itemName}`}>
           <Overlay visible={showOverlay && visible} onOverlayClick={handleOverlayClick} />
         </div>
-      </CSSTransition>
-      <div id="container" ref={menuRef} className={`${menuName}__bar`} style={{ zIndex: zIndexComputed }}>
+      </animated.div>
+      <div
+        id="container"
+        ref={menuRef}
+        className={`${menuName}__bar`}
+        style={{ zIndex: curIndex !== null ? zIndexComputed : 1 }}
+      >
         {newChildren.map((item: ReactElement, index) => (
           <div
             key={index}
@@ -84,14 +133,18 @@ const DropdownMenu: React.FC<DropdownMenuProps> = (props) => {
         ))}
       </div>
       {visible && (
-        <div
+        <animated.div
+          style={{
+            height: o.to((o) => `calc(100vh - ${o}px`),
+            transform: `translate(0px, ${top}px)`,
+            maxHeight: `calc(100vh - ${top}px)`,
+          }}
           className={classnames(`${name}-item`, {
             't-is-expanded': visible,
           })}
-          style={{ top }}
         >
           {items}
-        </div>
+        </animated.div>
       )}
     </div>,
   );
--- src/dropdown-menu/_example/index.jsx
+++ src/dropdown-menu/_example/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DropdownMenu, DropdownItem } from 'tdesign-mobile-react';
 import TDemoBlock from '../../../site/mobile/components/DemoBlock';
 import TDemoHeader from '../../../site/mobile/components/DemoHeader';
@@ -15,12 +15,12 @@ export default function () {
 
   const numberArrA = emptyArr.map((_, i) => ({
     label: `选项 ${String.fromCharCode(str.charCodeAt() + i)}`,
-    value: `option_${i + 1}`,
+    value: `option_${String.fromCharCode(str.charCodeAt() + i)}`,
   }));
 
   const numberArrB = emptyArr.map((_, i) => ({
     label: `选项 ${i + 3}`,
-    value: `option_${i + 1}`,
+    value: `option_${i + 3}`,
   }));
 
   const buildTree = (length) => {
@@ -28,12 +28,36 @@ export default function () {
     for (let i = 0; i < length; i++) {
       const item = {
         label: `选项 ${i + 1}`,
-        value: `options_${i}`,
+        value: `options_${i + 1}`,
       };
 
       const options = new Array(10).fill(null).map((_, index) => ({
         label: `子选项 ${index + 1 + i}`,
-        value: `options_${i}_${index}`,
+        value: `options_${1 + i}_${index + 1 + i}`,
+      }));
+
+      item.options = options;
+
+      tree.push(item);
+    }
+    return tree;
+  };
+
+  const buildTreeDepth3 = (length) => {
+    const tree = [];
+    for (let i = 0; i < length; i++) {
+      const item = {
+        label: `选项 ${i + 1}`,
+        value: `options_${i + 1}`,
+      };
+
+      const options = new Array(10).fill(null).map((_, index) => ({
+        label: `子选项 ${index + 1 + i}`,
+        value: `options_${i + 1}_${index + 1 + i}`,
+        options: new Array(10).fill(null).map((_, j) => ({
+          label: `孙子项 ${j + 1 + i + index}`,
+          value: `options_${i + 1}_${index + 1 + i}_${j + 1 + i + index}`,
+        })),
       }));
 
       item.options = options;
@@ -44,6 +68,21 @@ export default function () {
   };
 
   const optionsT1 = buildTree(8);
+  console.log('optionsT1: ', optionsT1);
+
+  const optionsT3 = buildTreeDepth3(6);
+  console.log('optionsT3: ', optionsT3);
+
+  const [value1, setValue1] = useState('');
+
+  const onChange1 = (value) => {
+    console.log(value);
+    if (!value[1]) {
+      setValue1(value[0]);
+    } else {
+      setValue1(value);
+    }
+  };
 
   return (
     <div className="tdesign-mobile-demo">
@@ -51,32 +90,32 @@ export default function () {
         title="DropdownMenu 下拉菜单"
         summary="菜单呈现数个并列的选项类目，用于整个页面的内容筛选，由菜单面板和菜单选项组成。"
       />
-      <TDemoBlock title="01 类型" summary="单选下拉菜单">
+      <TDemoBlock summary="单选单行下拉菜单">
         <DropdownMenu activeColor="#450">
-          <DropdownItem label="菜单" options={numberArr} />
-          <DropdownItem label="菜单" options={optionsT1} optionsLayout="tree" optionsColumns={2} />
-          <DropdownItem label="最多" options={numberArrB} optionsLayout="tree" optionsColumns={3} />
+          <DropdownItem label="菜单1" options={numberArr} defaultValue="option_3" onChange={onChange1} />
+          <DropdownItem label="菜单1" options={numberArrA} value={value1} onChange={onChange1} />
+          <DropdownItem label="最多1" options={numberArrB} onChange={onChange1} />
         </DropdownMenu>
       </TDemoBlock>
-      <TDemoBlock summary="多选下拉菜单">
+      <TDemoBlock title="01 类型" summary="树形单选多行下拉菜单">
         <DropdownMenu activeColor="#450">
-          <DropdownItem label="菜单1" multiple options={numberArr} />
-          <DropdownItem label="菜单1" multiple options={numberArrA} />
-          <DropdownItem label="最多1" multiple options={numberArrB} />
+          <DropdownItem label="菜单" options={numberArr} onChange={onChange1} />
+          <DropdownItem label="菜单" options={optionsT1} optionsLayout="tree" optionsColumns={2} onChange={onChange1} />
+          <DropdownItem label="最多" options={optionsT3} optionsLayout="tree" optionsColumns={3} onChange={onChange1} />
         </DropdownMenu>
       </TDemoBlock>
-      <TDemoBlock summary="多选下拉菜单">
+      <TDemoBlock summary="多选多行下拉菜单">
         <DropdownMenu activeColor="#450">
-          <DropdownItem label="菜单2" multiple options={numberArr} />
-          <DropdownItem label="菜单2" multiple options={numberArrA} />
-          <DropdownItem label="最多2" multiple options={numberArrB} />
+          <DropdownItem label="菜单2" multiple options={numberArr} onChange={onChange1} />
+          <DropdownItem label="菜单2" multiple options={optionsT1} onChange={onChange1} optionsColumns={2} />
+          <DropdownItem label="最多2" multiple options={numberArrB} onChange={onChange1} optionsColumns={3} />
         </DropdownMenu>
       </TDemoBlock>
-      <TDemoBlock summary="多选下拉菜单">
+      <TDemoBlock summary="树形多选下拉菜单">
         <DropdownMenu activeColor="#450">
-          <DropdownItem label="菜单3" multiple options={numberArr} />
-          <DropdownItem label="菜单3" multiple options={numberArrA} />
-          <DropdownItem label="最多3" multiple options={numberArrB} />
+          <DropdownItem label="菜单3" multiple optionsLayout="tree" options={optionsT1} />
+          <DropdownItem label="菜单3" multiple optionsLayout="tree" options={numberArrA} />
+          <DropdownItem label="最多3" multiple optionsLayout="tree" options={numberArrB} />
         </DropdownMenu>
       </TDemoBlock>
       <TDemoBlock summary="多选下拉菜单">
